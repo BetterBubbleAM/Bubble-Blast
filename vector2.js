b85e4c16f396e7701e68b6c71f3b4f94d1d010a5
BetterBubbleAM <legendakekee@gmail.com>
2026-03-12 01:47:18 +0100

    Delete vector2.js

--- vector2.js
+++
@@ -1,9 +0,0 @@
-class Vector2 {
-    constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
-    }
-    add(v) { this.x += v.x; this.y += v.y; return this; }
-    mult(n) { this.x *= n; this.y *= n; return this; }
-    copyFrom(v) { this.x = v.x; this.y = v.y; }
-}
